397b6273909b34b4df868c2e024319197530f509
Mujtaba Aslam <mujtaba@Mujtabas-MacBook-Pro.local>
2018-01-31 23:41:02 -0600

    hooks test 2

--- githooks/hooks-install.sh
+++
@@ -1,9 +0,0 @@
-HOOKS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-REPO_DIR="$(git rev-parse --show-toplevel)"
-if [[ -z $GIT_DIR ]]; then
-  GIT_DIR=".git"
-fi
-DOT_GIT_DIR="$REPO_DIR/$GIT_DIR"
-
-ln -sf $HOOKS_DIR/pre-commit $DOT_GIT_DIR/hooks/pre-commit
