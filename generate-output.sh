e52c2857c38e2b4015aa1ae5d83ba573a9487ee7
Mujtaba Aslam <mujtaba@Mujtabas-MacBook-Pro.local>
2018-02-15 21:06:26 -0600

    Added tests for compiler

--- generate-output.sh
+++ generate-output.sh
@@ -4,3 +4,5 @@ echo "===== test2 ====="
 ./cli this should print line by line
 echo "===== test3 ====="
 ./cli -length now give me lengths of this
+echo "===== test4 ====="
+./compiler "example.txt"
--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 
 ocamlc -o cli "cli.ml"
-
+ocamlopt -c lexer.ml
+ocamlopt -c lang.ml
+ocamlopt -c parser.ml
+ocamlopt -c compiler.ml
+ocamlopt -o compiler lexer.cmx lang.cmx parser.cmx compiler.cmx
 ./generate-output.sh > results.out
 diff solutions.out results.out
