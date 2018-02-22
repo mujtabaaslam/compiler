fe8353ea749e8db56c95630706126fa9360eaf98
Mujtaba Aslam <mujtaba@Mujtabas-MacBook-Pro.local>
2018-02-22 04:11:08 -0600

    added test for lex and parse

--- generate-output.sh
+++ generate-output.sh
@@ -6,3 +6,7 @@ echo "===== test3 ====="
 ./cli -length now give me lengths of this
 echo "===== test4 ====="
 ./compiler "example.txt"
+echo "===== test5 ====="
+./compiler -lex "example.txt"
+echo "===== test6 ====="
+./compiler -parse "example.txt"
