fc2676c707e68d072c62f213ca75d55797d72f06
Mujtaba Aslam <mujtaba@Mujtabas-MacBook-Pro.local>
2018-03-06 02:45:26 -0600

    added more tests

--- test/generate-output.sh
+++ test/generate-output.sh
@@ -1,7 +1,18 @@
 echo "===== test1 ====="
-./compiler.native example.txt 
+./compiler.native example.txt
 echo "===== test2 ====="
-./compiler.native -lex example.txt 
+./compiler.native -lex example.txt
 echo "===== test3 ====="
-./compiler.native -parse example.txt 
-
+./compiler.native -parse example.txt
+echo "===== test4 ====="
+./compiler.native test/test1.txt
+echo "===== test5 ====="
+./compiler.native -lex test/test1.txt
+echo "===== test6 ====="
+./compiler.native -parse test/test1.txt
+echo "===== test7 ====="
+./compiler.native test/test2.txt
+echo "===== test8 ====="
+./compiler.native -lex test/test2.txt
+echo "===== test9 ====="
+./compiler.native -parse test/test2.txt 
