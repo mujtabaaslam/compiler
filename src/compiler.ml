58dbb6528e7058818cca4895d6edff08a5f2e83c
Mujtaba Aslam <mujtaba@Mujtabas-MacBook-Pro.local>
2018-03-17 00:14:33 -0500

    added repl mode

--- src/compiler.ml
+++ src/compiler.ml
@@ -6,6 +6,7 @@ let lex = ref false
 let parse = ref false
 let file = ref ".txt"
 let step = ref false
+let repl = ref false
 
 let string_of_token (t:token) : string =
   match t with
@@ -62,36 +63,74 @@ let string_of_token_list (toks:token list) : string =
 let start_up(f:string) =
   file := f
 
-  let compile (file:string) =
-    let lexbuf = (Lexing.from_channel (open_in file)) in
-    if !lex then
-      let rec lexing tokens =
-        let t = Lexer.token lexbuf in
-          match t with
-          | Parser.EOF -> Printf.printf "["; Printf.printf "%s" (string_of_token_list (List.rev tokens)); Printf.printf "]\n"
-          | _ -> lexing (t :: tokens)
-          in lexing []
+let read () : string =
+  print_string "# "; flush stdout;
+  let rec create_list acc =
+    let ch = input_char stdin in
+    if ch = '\n' then
+      String.concat "" (List.map (String.make 1) (List.rev acc))
     else
-    let ast = Parser.prog Lexer.token lexbuf in
-      if !parse then
-        string_of_exp Env.empty ast |> print_endline
-      else if !step then
-        begin
+      create_list (ch :: acc)
+    in
+    create_list []
+
+let inter ast =
+  let typ = Lang.type_check ast in
+  let ans = Lang.interpret ast in
+  let value = (string_of_exp (left ans) (right ans)) in
+    Printf.printf "- : %s = %s\n" (string_of_typ typ) value
+
+let rec repl_mode () =
+  let input = read () in
+  if input = "exit" then ()
+  else
+      try
+        input
+        |> Lexing.from_string
+        |> Parser.prog Lexer.token
+        |> inter; repl_mode ()
+      with
+      | _ -> print_endline "Error"; repl_mode ()
+
+  let compile (file:string) =
+    if !repl then
+    begin
+    Printf.printf "        Compiler version 1.00\n";
+    repl_mode()
+    end
+    else
+      begin
+      let lexbuf = (Lexing.from_channel (open_in file)) in
+      if !lex then
+        let rec lexing tokens =
+          let t = Lexer.token lexbuf in
+            match t with
+            | Parser.EOF -> Printf.printf "["; Printf.printf "%s" (string_of_token_list (List.rev tokens)); Printf.printf "]\n"
+            | _ -> lexing (t :: tokens)
+            in lexing []
+      else
+      let ast = Parser.prog Lexer.token lexbuf in
+        if !parse then
+          string_of_exp Env.empty ast |> print_endline
+        else if !step then
+          begin
           type_check ast |> ignore;
           step_interpret ast
-        end
-      else
-        begin
-        type_check ast |> ignore;
-        let state = interpret ast in
-        string_of_exp (left state) (right state) |> print_endline
-        end
+          end
+        else
+          begin
+          type_check ast |> ignore;
+          let state = interpret ast in
+          string_of_exp (left state) (right state) |> print_endline
+          end
+      end
 
 let main () =
   let speclist = [
   ("-lex", Arg.Set lex, "processes the input source file through the lexing phase and prints the resulting stream of tokens to the console");
   ("-parse", Arg.Set parse, "processes the input source file through the parsing phase and prints the resulting abstract syntax tree");
-  ("-step", Arg.Set step, "processes the input and prints out every step of evaluation")
+  ("-step", Arg.Set step, "processes the input and prints out every step of evaluation");
+  ("-repl", Arg.Set repl, "enters repl mode which allows you to interactively enter expressions and see their final evaluated values")
   ] in
   let usage_msg = "Usage: my-project [flags] [args]\n Available flags:" in
   Arg.parse speclist start_up usage_msg;
